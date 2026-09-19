80a56338499e6270409fef6ebb6d548f1d854ed9
Ranjan7210 <ranjan721.singh@gmail.com>
2026-09-20 01:34:42 +0530

    Add Session 6 class problems and assignment folder structure

---
+++ src/session_6_inheritance_and_polymorphism/class_problem/StudentMember.java
@@ -0,0 +1,24 @@
+package session_6_inheritance_and_polymorphism.class_problem;
+
+public class StudentMember extends LibraryMember {
+    protected String course;
+
+    public StudentMember(String memberId, int borrowLimit, String course) {
+        super(memberId, borrowLimit);
+        this.course = course;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    @Override
+    protected void chargeFine(int amount) {
+        super.chargeFine(amount / 2);
+    }
+
+    @Override
+    public String displayInfo() {
+        return "Student Member | Course: " + course + " | Books Borrowed: " + booksBorrowed;
+    }
+}
