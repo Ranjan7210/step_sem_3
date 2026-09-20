1b6d54d5772e81fdd1c33e46c45040d8e3f9d892
Ranjan7210 <ranjan721.singh@gmail.com>
2026-09-20 13:05:12 +0530

    .

--- src/session_3_oop_basics/class_problem/IdCard.java
+++ src/session_3_oop_basics/class_problem/IdCard.java
@@ -1,4 +1,4 @@
-package Day3;
+package session_3_oop_basics.class_problem;
 
 import java.util.Scanner;
 
@@ -13,14 +13,14 @@ public class IdCard {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         String line1 = sc.nextLine();
         String[] parts1 = line1.split(", ");
         String name = parts1[0];
         int initialBooks = Integer.parseInt(parts1[1]);
-        
+
         int newBooks = Integer.parseInt(sc.nextLine());
-        
+
         String line3 = sc.nextLine();
         String[] parts3 = line3.split(", ");
         String separateName = parts3[0];
@@ -29,13 +29,13 @@ public class IdCard {
         IdCard ravi = new IdCard(name, initialBooks);
         IdCard duplicate = ravi;
         duplicate.booksIssued = newBooks;
-        
+
         System.out.println(ravi.name + "'s booksIssued (via first variable): " + ravi.booksIssued);
         System.out.println("duplicate == ravi: " + (duplicate == ravi));
-        
+
         IdCard separate = new IdCard(separateName, separateBooks);
         System.out.println("separate == ravi: " + (separate == ravi));
-        
+
         sc.close();
     }
 }
